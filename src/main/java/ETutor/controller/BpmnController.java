d99aff04fa4f60e4f6221d567b9f9fb33678783f
Dominik <dschuhmann96@gmail.com>
2022-04-06 16:35:43 +0200

    Initial commit

---
+++ src/main/java/ETutor/controller/BpmnController.java
@@ -0,0 +1,28 @@
+package ETutor.controller;
+
+import ETutor.Application;
+import ETutor.services.BpmnService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/bpmn")
+public class BpmnController {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private BpmnService bpmnService;
+
+    public BpmnController(BpmnService service) {
+        this.bpmnService = service;
+    }
+
+    @GetMapping("")
+    public String testruntime() {
+        return bpmnService.validate();
+    }
+
+}
---
+++ src/main/java/ETutor/services/BpmnService.java
@@ -0,0 +1,46 @@
+package ETutor.services;
+
+import ETutor.Application;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.task.TaskQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BpmnService {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private ProcessEngine processEngine;
+
+    public String validate() {
+        RuntimeService runtime = processEngine.getRuntimeService();
+        ProcessInstance instance = runtime.startProcessInstanceByKey("BPMN-Modul-process");
+        TaskService taskservice = processEngine.getTaskService();
+        TaskQuery taskQuery = taskservice.createTaskQuery();
+        Task task = taskQuery.singleResult();
+        logger.info(task.getName());
+        boolean result = this.checkName(task, "a");
+        logger.info(String.valueOf(result));
+        logger.info("Before " + taskQuery.list().toString());
+        taskservice.complete(task.getId());
+        Task task2 = taskQuery.singleResult();
+        logger.info(task2.getName());
+        boolean result2 = this.checkName(task2, "b");
+        taskservice.complete(task2.getId());
+        logger.info("After " + taskQuery.list().toString());
+
+        return String.valueOf(result + "   " + result2);
+    }
+
+    private boolean checkName(Task task, String solution) {
+        return task.getName().equals(solution);
+    }
+}
